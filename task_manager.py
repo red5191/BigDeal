93889429038c7c9bbf8059e86f625016cf27e50b
red5191 <the_red51@outlook.com>
2024-11-27 20:50:38 +0300

    123

--- task_manager.py
+++ task_manager.py
@@ -1,20 +1,31 @@
 class TaskManager():
     def __init__(self):
         self.tasklist = []
+        self.task = {}
 
     def add_task(self, description : str):
-        task = {description : str, complete : False}
-        self.tasklist.append(task)
+        self.task = {'description' : description, 'completed' : False}
+        self.tasklist.append(self.task)
 
     def complete_task(self, index: int):
-        self.tasklist[index[complete]] = True
+        try:
+            self.tasklist[index]['completed'] = True
+        except (IndexError, TypeError):
+            print('Задача не найдена')
 
-    # def remove_task(self, index: int)
-    #
+    def remove_task(self, index: int):
+        try:
+            del self.tasklist[index]
+        except (IndexError, TypeError):
+            print('Задача не найдена')
+
+
     # def save_to_json(self, filename: str)
     #
     # def load_from_json(self, filename: str)
 
-man1 = TaskManager
-man1.add_task()
-print(man1)
+man1 = TaskManager()
+
+man1.add_task(12345)
+man1.complete_task(1)
+man1.remove_task('fdgv')
