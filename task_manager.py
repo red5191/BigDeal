40e80c732b8ddff74a098b91ad960229d909adbd
red5191 <the_red51@outlook.com>
2024-12-01 18:57:38 +0300

    noJSON

--- task_manager.py
+++ task_manager.py
@@ -1,43 +1,45 @@
 import json
 
 
-class TaskManager():
+class TaskManager:
     def __init__(self):
         self.tasklist = []
-        self.task = {}
 
     def add_task(self, description: str):
-        for self.task in self.tasklist:
-            if description == self.task['description']:
-                return print(f'Задача {self.task['description']} уже есть в списке.')
+        for task in self.tasklist:
+            if description == task['description']:
+                return print(f'Задача {task['description']} уже есть в списке.')
 
-        self.task = {'description': description, 'completed': False}
-        self.tasklist.append(self.task)
-        print(f'Задача {self.task['description']} добавлена в список.')
+        new_task = {'description': description, 'completed': False}
+        self.tasklist.append(new_task)
+        print(f'Задача {new_task['description']} добавлена в список.')
 
     def complete_task(self, index: int):
         try:
             if self.tasklist[index]['completed']:
-                print(f'Задача {self.task['description']} уже помечена как выполненная')
+                print(f'Задача {self.tasklist[index]['description']} уже помечена как выполненная.')
             else:
                 self.tasklist[index]['completed'] = True
         except (IndexError, TypeError):
-            print('Задача не найдена')
+            print('Задача не найдена.')
 
     def remove_task(self, index: int):
         try:
-            print(f'Задача {self.tasklist[index]['description']} удалена')
+            print(f'Задача {self.tasklist[index]['description']} удалена.')
             del self.tasklist[index]
         except (IndexError, TypeError):
-            print('Задача не найдена')
+            print('Задача не найдена.')
 
     def save_to_json(self, filename: str):
         with open(filename, 'w') as file:
             json.dump(self.tasklist, file, indent=4)
+        print('Список задач сохранен в json файл.')
 
-    def load_from_json(self, filename: str):
+    @staticmethod
+    def load_from_json(filename: str):
         with open(filename, 'r') as file:
             from_file = json.load(file)
+        print('Список задач выгружен из json файла.')
         return from_file
 
     def get_list(self):
@@ -46,8 +48,8 @@ class TaskManager():
 
 manager1 = TaskManager()
 
-manager1.add_task(12345)
-manager1.add_task(12345)
+manager1.add_task('totoro')
+manager1.add_task('totoro')
 manager1.add_task('aboba')
 manager1.add_task('zizi')
 manager1.complete_task(1)
@@ -56,3 +58,5 @@ manager1.remove_task(0)
 manager1.remove_task(2)
 manager1.save_to_json('tasklist.json')
 manager1.load_from_json('tasklist.json')
+manager1.get_list()
+
